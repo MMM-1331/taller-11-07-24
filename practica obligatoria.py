87e5a5edab9a6ed5ddd1626b7207856d950dffb1
MMM-1331 <m.m.m.13@hotmail.es>
2024-07-11 22:50:03 -0500

    Add files via upload

---
+++ practica obligatoria.py
@@ -0,0 +1,20 @@
+#datos
+nombre = input("cual es tu nombre: ")
+#pide e imprime nombre
+aMo = int((input("en que año naciste: ")))
+EDAD = 2024-aMo
+
+#print(f"tu tienes {EDAD} años")
+#calcula edad con año de nacimiento
+
+curso = input("que estas estudiando: ")
+
+print(f"Bienvenido {nombre}, exelente que a tus {EDAD} años estas estudiando {curso}, ¡¡¡exitos!!!")
+#pide nombre, año de nacimiento y curso
+
+numero = input("digita un numero aleatorio: ")
+entero = int(numero)
+#pide numero aleatorio y lo convierte a entero
+
+print(entero * 2)
+print(type(entero))
